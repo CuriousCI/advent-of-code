2bd14012d63656a1bf97d49be69f2ccc9f9bd4d8
Ionut Cicio <ionutcicio.dev@gmail.com>
2022-09-18 14:29:05 +0200

    Delete 2015/3/target directory

--- 2015/3/target/debug/aoc.d
+++
@@ -1,1 +0,0 @@
-/home/cicio/dev/advent-of-code/2015/3/target/debug/aoc: /home/cicio/dev/advent-of-code/2015/3/src/input.txt /home/cicio/dev/advent-of-code/2015/3/src/main.rs
--- 2015/3/target/debug/deps/aoc-5878996cd93f6a64.d
+++
@@ -1,6 +0,0 @@
-/home/cicio/dev/advent-of-code/2015/3/target/debug/deps/aoc-5878996cd93f6a64: src/main.rs src/input.txt
-
-/home/cicio/dev/advent-of-code/2015/3/target/debug/deps/aoc-5878996cd93f6a64.d: src/main.rs src/input.txt
-
-src/main.rs:
-src/input.txt:
