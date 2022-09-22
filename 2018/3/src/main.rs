c89d71f05e31cf9c5f0925dd56a1ae5c58aac0f2
Ionut Cicio <ionutcicio.dev@gmail.com>
2022-09-22 20:52:41 +0200

    2018 3

--- 2018/3/src/main.rs
+++ 2018/3/src/main.rs
@@ -30,7 +30,7 @@ impl FromStr for Claim {
 }
 
 fn part_one() -> i32 {
-    let claims: Vec<Claim> = include_str!("example")
+    let claims: Vec<Claim> = include_str!("input")
         .trim()
         .lines()
         .map(str::parse)
@@ -41,7 +41,7 @@ fn part_one() -> i32 {
         (min(start, claim.x), max(end, claim.x + claim.width))
     });
 
-    let area = 0;
+    let mut area = 0;
 
     for x in start..end {
         let mut intervals: Vec<(i32, i32)> = claims
@@ -53,7 +53,31 @@ fn part_one() -> i32 {
         if intervals.len() > 1 {
             intervals.sort();
 
-            let _intersections: Vec<(i32, i32)>;
+            let mut intersections: Vec<(i32, i32)> = vec![];
+            for index in 1..intervals.len() {
+                if intervals[index - 1].1 >= intervals[index].0 {
+                    intersections.push((intervals[index].0, intervals[index - 1].1));
+                }
+            }
+
+            intersections.sort();
+
+            let mut intersection = 0;
+            for index in 1..intersections.len() {
+                if intersections[index].0 <= intersections[intersection].1 {
+                    intersections[intersection].1 =
+                        max(intersections[index].1, intersections[intersection].1);
+                    intersections[index] = (0, 0);
+                } else {
+                    intersection = index;
+                }
+            }
+
+            area += intersections
+                .iter()
+                .filter(|(y, height)| *y != 0 && *height != 0)
+                .map(|(y, height)| height - y + 1)
+                .sum::<i32>();
         }
     }
 
