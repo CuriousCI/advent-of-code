6ecfaa1767e0b52d9f553964707899af574de8f8
Ionut Cicio <ionutcicio.dev@gmail.com>
2022-12-01 20:58:49 +0100

    2022 1 2

--- 2022/1/src/main.rs
+++ 2022/1/src/main.rs
@@ -13,6 +13,24 @@ fn part_one() -> u32 {
         .unwrap()
 }
 
+fn part_two() -> u32 {
+    let mut elves_calories: Vec<u32> = include_str!("input")
+        .trim()
+        .split("\n\n")
+        .map(|elf| {
+            elf.trim()
+                .split('\n')
+                .map(str::parse::<u32>)
+                .map(Result::unwrap)
+                .sum()
+        })
+        .collect();
+
+    elves_calories.sort();
+    elves_calories.reverse();
+    elves_calories.iter().take(3).sum()
+}
+
 fn main() {
-    println!("{}", part_one())
+    println!("{}", part_two())
 }
