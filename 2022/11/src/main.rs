eb140c2efc66fb30cf2bdb440eaa7819e7e2a295
Ionut Cicio <ionutcicio.dev@gmail.com>
2022-12-11 21:11:36 +0100

    2022 11 1

---
+++ 2022/11/src/main.rs
@@ -0,0 +1,17 @@
+struct Monkey {
+    items: Vec<u32>,
+    operator: String,
+    operand: u32,
+    test: u32,
+    falsy: u32,
+    truthy: u32,
+    operations: u32,
+}
+
+fn part_one() -> u32 {
+    10
+}
+
+fn main() {
+    println!("{}", part_one());
+}
