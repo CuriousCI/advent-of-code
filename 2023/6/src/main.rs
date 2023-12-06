87df56bd81049e909127dc1736b17f44d17c1bd3
Ionut Cicio <ionutcicio.dev@gmail.com>
2023-12-06 23:43:36 +0100

    2023 6

--- 2023/6/src/main.rs
+++ 2023/6/src/main.rs
@@ -1,7 +1,7 @@
 fn part_one() -> u64 {
     let (time, distance) = include_str!("input").split_once('\n').unwrap();
 
-    let time: Vec<u64> = time
+    let time: Vec<f64> = time
         .strip_prefix("Time:")
         .unwrap()
         .split_whitespace()
@@ -9,7 +9,7 @@ fn part_one() -> u64 {
         .map(Result::unwrap)
         .collect();
 
-    let distance: Vec<u64> = distance
+    let distance: Vec<f64> = distance
         .strip_prefix("Distance:")
         .unwrap()
         .split_whitespace()
@@ -18,15 +18,43 @@ fn part_one() -> u64 {
         .collect();
 
     let mut result = 1;
-    for (time, distance) in time.iter().zip(distance) {
-        let delta = time.pow(2) - 4 * distance;
-        let upper = -time + sqrt(delta);
-        //
+
+    for (&time, distance) in time.iter().zip(distance) {
+        let delta = time.powi(2) - 4.0 * distance;
+        let upper = (-1.0 * delta.sqrt() - time) / -2.0;
+        let lower = (delta.sqrt() - time) / -2.0;
+
+        result *= (upper.ceil() - lower.floor()) as u64 - 1
     }
 
-    result
+    result as u64
+}
+
+fn part_two() -> u64 {
+    let (time, distance) = include_str!("input").split_once('\n').unwrap();
+
+    let time: f64 = time
+        .chars()
+        .filter(|ch| ch.is_digit(10))
+        .collect::<String>()
+        .parse()
+        .unwrap();
+
+    let distance: f64 = distance
+        .chars()
+        .filter(|ch| ch.is_digit(10))
+        .collect::<String>()
+        .parse()
+        .unwrap();
+
+    let delta = time.powi(2) - 4.0 * distance;
+    let upper = (-1.0 * delta.sqrt() - time) / -2.0;
+    let lower = (delta.sqrt() - time) / -2.0;
+
+    (upper.ceil() - lower.floor()) as u64 - 1
 }
 
 fn main() {
     println!("{}", part_one());
+    println!("{}", part_two());
 }
